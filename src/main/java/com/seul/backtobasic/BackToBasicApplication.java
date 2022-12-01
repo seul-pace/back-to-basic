cdc5238435f143d578d694292afa54fcff03d69d
semo-project <62363111+semo-project@users.noreply.github.com>
2022-12-01 20:35:36 +0900

    first commit

---
+++ src/main/java/com/seul/backtobasic/BackToBasicApplication.java
@@ -0,0 +1,13 @@
+package com.seul.backtobasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackToBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackToBasicApplication.class, args);
+	}
+
+}
