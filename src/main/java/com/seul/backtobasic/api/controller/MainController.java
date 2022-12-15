51ea072e130bbf80846f0ed7e953ead03c59803f
semo-project <62363111+semo-project@users.noreply.github.com>
2022-12-15 20:19:29 +0900

    feat: Main 추가, yml 파일로 변경

---
+++ src/main/java/com/seul/backtobasic/api/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.seul.backtobasic.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "test";
+    }
+
+}
